0246ca616184e5b380b6de94c93482f826f2b8cc
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2017-01-22 16:39:27 -0600

    remove boilerplate code, add dateFns to project, add basic timeline to project

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,146 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>{{ title }}</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>START</th>
+          <th>END</th>
+          <th>CURRENT</th>
+          <th>HOURS IN DAY</th>
+          <th>HOURS LEFT</th>
+          <th>HOURS SPENT</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ times.start }}</td>
+          <td>{{ times.end }}</td>
+          <td>{{ times.current }}</td>
+          <td>{{ times.hoursInDay }}</td>
+          <td>{{ times.hoursLeftInDay }}</td>
+          <td>{{ times.hoursSpentInDay }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <section class="timeline">
+      <div v-for="hours in times.hoursArray" class="timeline__hour"></div>
+    </section>
   </div>
 </template>
 
 <script>
+import dateFns from 'date-fns'
+
+let start = dateFns.subHours(new Date(), 5);
+let end = dateFns.addHours(new Date(), 3);
+let current = new Date();
+let hoursInDay = dateFns.differenceInHours(start, end);
+let hoursLeftInDay = dateFns.differenceInHours(current, end);
+let hoursSpentInDay = dateFns.differenceInHours(start, current);
+
+start = format(start);
+end = format(end);
+current = format(current);
+hoursInDay = Math.abs(hoursInDay);
+hoursLeftInDay = Math.abs(hoursLeftInDay);
+hoursSpentInDay = Math.abs(hoursSpentInDay);
+
+let hoursArray = [];
+
+for (var i = 0; i < hoursSpentInDay; i++) {
+  hoursArray.push(i+1);
+}
+
+const times = {
+  start,
+  end,
+  current,
+  hoursInDay,
+  hoursLeftInDay,
+  hoursSpentInDay,
+  hoursArray,
+}
+
+function format(date) {
+  return dateFns.format(date, 'HH:mm');
+}
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'Welcome to DayPlanner',
+      times,
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+$primary-color: #2c2c2c;
+$white: #fff;
+$primary-font: "HelveticaNeue";
+$body-font-weight: 100;
+$header-font-weight: 600;
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
 }
 
-h1, h2 {
-  font-weight: normal;
+body {
+  background-color: $primary-color;
+  color: $white;
+  display: flex;
+  // justify-content: center;
+  font-family: $primary-font;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  font-size: 28px;
+  @extend %headingFont;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+%bodyFont {
+  font-weight: $body-font-weight;
+  margin-bottom: 50px;
 }
 
-a {
-  color: #42b983;
+%headingFont {
+  font-weight: $header-font-weight;
+  margin-bottom: 50px;
 }
+
+table {
+  width: 100%;
+}
+
+th {
+  @extend %headingFont;
+  width: 16%;
+  text-align: left;
+}
+
+td {
+  @extend %bodyFont;
+  width: 16%;
+  text-align: left;
+}
+
+.timeline {
+  margin-top: 50px;
+  background-color: #3473d6;
+  width: 800px;
+  height: 10px;
+}
+
+.timeline__hour {
+  width: 100px;
+  background-color: #e74430;
+  height: 10px;
+  float: left;
+  border-right: 1px solid #2c2c2c;
+}
+
 </style>
